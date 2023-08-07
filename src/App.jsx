4c60cb6cf0b97e163e9b8d03cc5c5876d959d2ec
Smail Bensaad <amsmailbensaad@gmail.com>
2023-08-07 12:55:50 +0200

    initialcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Form from "./Components/Form/Form.jsx";
+import List from "./Components/List/List.jsx";
+import FetchedWeather from "./Components/Fetch/Fetch";
+import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import { uid } from "uid";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [isGoodWeather, setIsGoodWeather] = useState(true);
+  // This uses the useState hook to define a state variable named isGoodWeather, which is initialized with the value true. 
+  // The setIsGoodWeather function is used to update this state.
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
+  // This uses the useLocalStorageState hook to define a state variable named activities. 
+  // It is initialized with the value retrieved from the browser's local storage under the key "activities". 
+  // If the key does not exist, it uses an empty array as the default value.
+  // The setActivities function is used to update this state.
+  // console.log(isGoodWeather);
+
+  // console.log(activities);
+  function changeWeather(newWeatherStatus) {
+    setIsGoodWeather(newWeatherStatus);
+  }
+  // This function takes a new weather status (true or false) as an argument and updates the isGoodWeather state with that value.
+
+  function changeActivities(newArray) {
+    setActivities(newArray);
+  }
+  // This function takes a new array of activities as an argument and updates the activities state with that value.
+
+  function onAddActivity({ activityInput, isForGoodWeather }) {
+    const newActivity = {
+      id: uid(),
+      activityText: activityInput,
+      isForGoodWeather: isForGoodWeather,
+    };
+    setActivities([...activities, newActivity]);
+  }
+// This function takes an object with two properties as an argument: activityInput and isForGoodWeather.
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section>
+      <FetchedWeather changeWeather={changeWeather} />
+      <List
+        activities={activities}
+        isGoodWeather={isGoodWeather}
+        changeActivities={changeActivities}
+      ></List>
+      <Form onAddActivity={onAddActivity} />
+    </section>
+  );
 }
-
-export default App
+// The return statement renders the App component's content inside a <section> element.
+// It includes the FetchedWeather component, which is passed the changeWeather function as a prop.
+// This component likely allows the user to fetch weather data and change the isGoodWeather state accordingly.
+// The List component is also rendered, which is passed the activities, isGoodWeather, and changeActivities props. 
+// This component likely displays the list of activities and allows the user to modify them.
+// Lastly, the Form component is rendered, which is passed the onAddActivity function as a prop. 
+// This component is likely used for user input to add new activities.
---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,55 @@
+import "./Form.css";
+export default function Form({ onAddActivity }) 
+{
+  function handleSubmitData(event) {
+    // This is a functional component named "Form" that takes a prop named "handleAddActivity". 
+    // The "handleAddActivity" prop is a function that will be used to add the new activity data when the form is submitted.//
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddActivity(data);
+    event.target.reset();
+    document.getElementById("activityInput").focus();
+  }
+  // This is an inner function defined within the "Form" component. 
+  // It handles the form submission when the user clicks the "Add" button. 
+  // It prevents the default form submission behavior, extracts the data from the form using FormData, converts it into a regular JavaScript object using "Object.fromEntries", 
+  // calls the "handleAddActivity" function with the data, resets the form, and focuses on the input field with the ID "activityInput".
+
+  return (
+    <form className="activity-form" onSubmit={handleSubmitData}>
+      {/* This is the main form element that wraps the entire form content. The "onSubmit" attribute is set to call the "handleSubmitData" function when the form is submitted. */}
+      <h2 className="activity-form__title">Add new Activity:</h2>
+      {/* This is a heading displaying the title "Add new Activity". */}
+      <div className="activity-form__new-activity">
+      {/* This div contains the input field for entering the new activity. */}
+        <label htmlFor="activityInput"></label>
+        {/* This empty label is used to associate with the input field with the ID "activityInput" for accessibility purposes. */}
+        <input
+          type="text"
+          id="activityInput"
+          name="activityInput"
+          minLength="3"
+          placeholder="Write Your Beloved Activity Here"
+        />
+        {/* This is the input field where the user can enter the new activity. The ID "activityInput" is associated with the label. It has a minimum length set to 3 characters, and it shows the placeholder text "Write Your Beloved Activity Here". */}
+      </div>
+      <div className="activity-checkbox">
+      {/* This div contains a checkbox input field for indicating whether the activity is suitable for good weather. */}
+        <label className="checkbox-label">Good-Weather Activity:</label>
+        {/* This label text describes the checkbox for indicating if the activity is suitable for good weather. */}
+        <input
+          className="checkbox"
+          type="checkbox"
+          id="isForGoodWeather"
+          name="isForGoodWeather"
+          // This is the checkbox input field where the user can indicate if the activity is suitable for good weather.
+        />
+      </div>
+      <button className="activity-button" type="submit">
+        Add
+      </button>
+      {/* This is the "Add" button that will trigger the form submission when clicked. */}
+    </form>
+  );
+}
